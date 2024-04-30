5c5328083521b3bbd06e7766a9ff9c02c0ffca10
SvedKatryu <ShvedKatryu@yandex.ru>
2024-04-30 12:04:01 +0300

    fix: удалена ненужная аннотация @responsebody

--- stats/stats-server/src/main/java/ru/practicum/explore_with_me/error/ErrorHandler.java
+++ stats/stats-server/src/main/java/ru/practicum/explore_with_me/error/ErrorHandler.java
@@ -3,7 +3,6 @@ package ru.practicum.explore_with_me.error;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MissingRequestValueException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.practicum.explore_with_me.error.exeption.BadRequestException;
@@ -19,14 +18,12 @@ public class ErrorHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
     public ErrorResponse handleValidationException(final MissingRequestValueException ex) {
         return new ErrorResponse(ex.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
     public ErrorResponse handleValidationException(final ValidationException ex) {
         return new ErrorResponse(ex.getMessage());
     }
